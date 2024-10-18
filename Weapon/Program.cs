ebdd710a4df3ed6a42c1abed9d8ad214b4ee4b6a
Toghrul-Mehdi <togrulcm-bp215@code.edu.az>
2024-10-18 22:07:40 +0400

    Weapon

---
+++ Weapon/Program.cs
@@ -0,0 +1,150 @@
+﻿
+namespace Weapon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool f = false;
+            bool b = false;
+
+
+            int capacity;
+            int bullet;
+            string firemode;
+
+            bool bool_capacity;
+
+            do
+            {
+                Console.WriteLine("Capacity daxil et:");
+                bool_capacity = int.TryParse(Console.ReadLine(), out capacity);
+
+                if (capacity > 0)
+                {
+                    bool_capacity = true;
+                }
+                else
+                {
+                    Console.WriteLine("Capacity 0dan boyuk olmalidir!");
+                    bool_capacity = false;
+                }
+            } while (!bool_capacity);
+            bool bool_bullet;
+            do
+            {
+                Console.WriteLine("Mermi sayi daxil et:");
+                bool_bullet = int.TryParse(Console.ReadLine(), out bullet);
+
+                if (bullet <= capacity)
+                {
+                    bool_bullet = true;
+                }
+                else
+                {
+                    Console.WriteLine("Mermi sayi daraq ucun yeterli deyil!");
+                    bool_bullet = false;
+                }
+            } while (!bool_bullet);
+            Console.WriteLine("Atis modu daxil edin:auto ve ya single");
+            firemode = Console.ReadLine();
+
+            Weapon Ak47 = new Weapon(capacity, bullet, firemode);
+            do
+            {
+                Console.WriteLine("0.Info 1.FireMode 2.GetRemainBullet  3.Reload 4.ChangeFireMode 5.Quit 6.Edit");
+
+                string operation = Console.ReadLine();
+                switch (operation)
+                {
+                    case "0":
+
+                        Ak47.Information();
+                        break;
+                    case "1":
+                        Ak47.Shoot();
+                        break;
+
+                    case "2":
+                        Ak47.GetRemainBullet();
+                        break;
+                    case "3":
+                        Ak47.Reload();
+                        break;
+                    case "4":
+                        Ak47.ChangeFireMode();
+                        break;
+                    case "5":
+                        f = true;
+                        break;
+
+
+                    case "6":
+                        while (true)
+                        {
+                            Console.WriteLine("\nYeni menu:");
+                            Console.WriteLine("7 - Tutumu deyisin");
+                            Console.WriteLine("8 - Gullə sayi dəyisin");
+                            Console.WriteLine("9 - Geri menuya qayit");
+                            Console.Write("Bir operation seçin: ");
+                            string editChoice = Console.ReadLine();
+
+                            if (editChoice == "9") break;
+
+                            switch (editChoice)
+                            {
+                                case "7":
+                                    Console.Write("Yeni tutumu daxil edin: ");
+                                    int newCapacity;
+                                    if (int.TryParse(Console.ReadLine(), out newCapacity) && newCapacity > 0)
+                                    {
+
+                                        Ak47.ChangeCap(newCapacity);
+                                        Console.WriteLine($"Yeni tutum: {newCapacity}");
+
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Yanlıs tutum dəyəri.");
+                                    }
+                                    break;
+
+                                case "8":
+                                    Console.Write("Yeni gulle sayını daxil edin: ");
+                                    int newBulletCount;
+                                    if (int.TryParse(Console.ReadLine(), out newBulletCount) && newBulletCount >= 0)
+                                    {
+                                        if (newBulletCount <= Ak47.Capacity)
+                                        {
+                                            Ak47.ChangeBullet(newBulletCount);
+                                            Console.WriteLine($"Yeni gulle sayı: {newBulletCount}");
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Yanlish gulle  sayı və ya tutum aşılır.");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Yanlish gulle sayı.");
+                                    }
+                                    break;
+
+                                default:
+                                    Console.WriteLine("Yanlish secim.");
+                                    break;
+                            }
+                        }
+                        break;
+
+
+
+                }
+
+            } while (!f);
+
+
+
+        }
+    }
+}
---
+++ Weapon/Weapon.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapon
+{
+    internal class Weapon
+    {
+        public int Capacity;
+        public int Bullet;
+        public string FireMode;
+
+
+        private int _capacity;
+
+        public int BulletCapacity
+        {
+            get { return _capacity; }
+
+            set
+            {
+                if (value > 0)
+                {
+                    _capacity = value;
+
+                }
+                
+            }
+        }
+
+        private static int _bullets;
+
+        public static int Bullets
+        {
+            get { return _bullets; }
+
+            set
+            {
+                if (value > 0)
+                {
+                    _bullets = value;
+                }
+            }
+        }
+
+        public Weapon(int capacity, int bullet, string firemode)
+        {
+            _bullets--;
+            Capacity = capacity;
+            Bullet = bullet;
+            FireMode = firemode;
+        }
+
+        public void Shoot()
+        {
+            if (FireMode == "auto" && Bullet > 0)
+            {
+                Console.WriteLine("Evvelki Mermi:" + Bullet);
+                Bullet = 0;
+                Console.WriteLine("Indiki Mermi:" + Bullet);
+            }
+            else if (FireMode == "single" && Bullet > 0)
+            {
+
+                Bullet--;
+                Console.WriteLine("Indiki Mermi:" + Bullet);
+            }
+            else
+            {
+                Console.WriteLine("Mellim zehmet olmasa Partdatmaga calismayin)");
+            }
+
+        }
+
+        public void Information()
+        {
+            Console.WriteLine("Capacity:" + Capacity);
+            Console.WriteLine("Mermi sayi:" + Bullet);
+            if (FireMode == "auto")
+            {
+                Console.WriteLine("FireMode:Auto");
+            }
+            else if (FireMode == "single")
+            {
+                Console.WriteLine("FireMode:Single");
+            }
+        }
+        public void GetRemainBullet()
+        {
+            if (Capacity >= Bullet)
+            {
+                int QalanMermi = Capacity - Bullet;
+                Console.WriteLine(QalanMermi + " lazimdiki tam olsun");
+            }
+            else
+            {
+                Console.WriteLine("Proqrami partdatmaq istedin? Alinmadi))");
+            }
+        }
+
+        public void Reload()
+        {
+            if (Capacity >= Bullet)
+            {
+                Bullet = Capacity;
+                Console.WriteLine("Mermi sayi:" + Bullet);
+            }
+            else
+            {
+                Console.WriteLine("Proqrami partdatmaq istedin? Alinmadi))");
+            }
+
+        }
+        public void ChangeFireMode()
+        {
+
+            if (FireMode == "auto")
+            {
+                FireMode = "single";
+                Console.WriteLine("Single Mode kecid edildi");
+            }
+            else if (FireMode == "single")
+            {
+                FireMode = "auto";
+                Console.WriteLine("Auto Mode kecid edildi");
+            }
+            else
+            {
+                Console.WriteLine("Proqrami partdatmaq istedin? Alinmadi))");
+            }
+        }
+
+
+        public void ChangeCap(int newcap)
+        {
+            if (Capacity == newcap)
+            {
+                Console.WriteLine("Tutum deyismedi!");
+            }
+            else if(Capacity !=newcap)
+            {
+                Console.WriteLine("Tutum deyisdi:");
+            }
+            Capacity=newcap;
+        }
+
+        public void ChangeBullet(int newbullet)
+        {
+            if(Bullet == newbullet)
+            {
+                Console.WriteLine("Gulle deyismedi");
+            }
+            else if(Bullet != newbullet)
+            {
+                Console.WriteLine("Gulle deyisdi");
+            }
+            Bullet=newbullet;
+        }
+
+        
+
+
+
+    }
+}
